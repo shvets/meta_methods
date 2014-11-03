a62203d3ac765eb03ec35e25825251cf5e86f043
ashvets <Sheila01>
2014-11-03 12:14:53 -0500

    Api changes

--- spec/core_spec.rb
+++ spec/core_spec.rb
@@ -1,10 +1,8 @@
 require 'meta_methods/core'
 
-class MetaMethodsDemo
-  include MetaMethods::Core
-end
+describe MetaMethods::Core do
+  let(:subject) { MetaMethods::Core.instance }
 
-describe MetaMethodsDemo do
   describe "#define_attribute" do
     it "creates new attribute on object" do
       object = Object.new
@@ -40,28 +38,4 @@ describe MetaMethodsDemo do
     end
   end
 
-  describe "#evaluate_dsl" do
-    it "calls all lambdas" do
-      create_block = lambda { created }
-      destroy_block = lambda {|object| p destroyed(object) }
-
-      execute_block = lambda {|object| p executed(object) }
-
-      subject.evaluate_dsl(create_block, destroy_block, execute_block)
-    end
-  end
-
-  private
-
-  def created
-    p "created"
-  end
-
-  def destroyed object
-    p "destroyed: #{object}"
-  end
-
-  def executed object
-    p "executed: #{object}"
-  end
 end
