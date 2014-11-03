b7cf30c5c14d7f2137a52cb7cc7ec9c70a94ecf3
Alexander Shvets <alexander.shvets@gmail.com>
2014-11-02 21:59:43 -0500

    bug fixes

--- lib/meta_methods/core.rb
+++ lib/meta_methods/core.rb
@@ -1,101 +1,74 @@
-module MetaMethods::Core
-  def metaclass object
-    # class << object
-    #   self
-    # end
-    object.singleton_class
-  end
+module MetaMethods
+  module Core
+    def define_attribute(object, key, value)
+      object.singleton_class.send :attr_accessor, key.to_sym # creates accessor
 
-  # def define_attributes(type, object, hash, create_instance=true)
-  #   hash.each_pair do |key, value|
-  #     define_attribute(type, object, key, value, create_instance)
-  #   end
-  # end
-  #
-  # def define_attribute(type, object, key, value, create_instance=true)
-  #   if create_instance
-  #     metaclass(object).send type, key.to_sym
-  #
-  #     object.instance_variable_set("@#{key}".to_sym, value)
-  #   else
-  #     metaclass(object).class_eval <<-CODE
-  #       def #{key}
-  #         "#{value}"
-  #       end
-  #     CODE
-  #   end
-  # end
-
-  def define_attribute(object, key, value)
-    object.singleton_class.send :attr_accessor, key.to_sym # creates accessor
-
-    object.send "#{key}=".to_sym, value  # sets up value for attribute
-  end
-
-  def define_attributes(object, hash)
-    hash.each_pair do |key, value|
-      define_attribute(object, key, value)
+      object.send "#{key}=".to_sym, value  # sets up value for attribute
     end
-  end
 
-  def locals_to_hash object, content
-    scope = object.instance_eval { binding }
+    def define_attributes(object, hash)
+      hash.each_pair do |key, value|
+        define_attribute(object, key, value)
+      end
+    end
 
-    eval(content, scope)
+    def locals_to_hash object, content
+      scope = object.instance_eval { binding }
 
-    extract_values(defined_vars(scope), scope)
-  end
+      eval(content, scope)
 
-  def evaluate_dsl(create_block, destroy_block, execute_block)
+      extract_values(defined_vars(scope), scope)
+    end
 
-    begin
-      created_object = create_block.kind_of?(Proc) ? create_block.call : create_block
+    def evaluate_dsl(create_block, destroy_block, execute_block)
+      begin
+        created_object = create_block.kind_of?(Proc) ? create_block.call : create_block
 
-      created_object.instance_variable_set(:@parent, block_parent(execute_block))
+        created_object.instance_variable_set(:@parent, block_parent(execute_block))
 
-      def created_object.method_missing(sym, *args, &block)
-        @parent.send sym, *args, &block
+        def created_object.method_missing(sym, *args, &block)
+          @parent.send sym, *args, &block
+        end
+
+        def created_object.respond_to?(sym, include_private = false)
+          @parent.respond_to? sym, include_private
+        end
+
+        created_object.instance_eval(&execute_block)
+      ensure
+        destroy_block.call(created_object) if destroy_block && created_object
+      end
+    end
+
+    def block_parent block
+      block_binding = block.binding
+
+      block_binding.eval 'self'
+    end
+
+    private
+
+    def defined_vars scope
+      eval("local_variables", scope) - Kernel.local_variables - %w(content)
+    end
+
+    def extract_values vars_list, scope
+      vars_values_list = vars_list.collect { |name| [name, get_property(name, scope)] }
+
+      hash = {}
+      vars_values_list.each do |var, value|
+        hash[var.to_sym] = value
       end
 
-      def created_object.respond_to?(sym, include_private = false)
-        @parent.respond_to? sym, include_private
+      hash
+    end
+
+    def get_property name, scope
+      begin
+        eval(name.to_s, scope)
+      rescue NameError
+        nil
       end
-
-      created_object.instance_eval(&execute_block)
-    ensure
-      destroy_block.call(created_object) if destroy_block && created_object
     end
   end
-
-  def block_parent block
-    block_binding = block.binding
-
-    block_binding.eval 'self'
-  end
-
-  private
-
-  def defined_vars scope
-    eval("local_variables", scope) - Kernel.local_variables - %w(content)
-  end
-
-  def extract_values vars_list, scope
-    vars_values_list = vars_list.collect { |name| [name, get_property(name, scope)] }
-
-    hash = {}
-    vars_values_list.each do |var, value|
-      hash[var.to_sym] = value
-    end
-
-    hash
-  end
-
-  def get_property name, scope
-    begin
-      eval(name.to_s, scope)
-    rescue NameError
-      nil
-    end
-  end
-
 end
--- spec/meta_methods_spec.rb
+++ spec/meta_methods_spec.rb
@@ -1,4 +1,4 @@
-require 'meta_methods'
+require 'meta_methods/core'
 
 class MetaMethodsDemo
   include MetaMethods::Core
@@ -19,9 +19,9 @@ describe MetaMethodsDemo do
     it "creates new attributes on object" do
       object = Object.new
 
-      subject.define_attributes object,
+      subject.define_attributes(object,
                                 {new_attribute1: "new_attribute_value1",
-                                 new_attribute2: "new_attribute_value2" }
+                                 new_attribute2: "new_attribute_value2" })
 
       expect(object.new_attribute1).to eq("new_attribute_value1")
       expect(object.new_attribute2).to eq("new_attribute_value2")
@@ -34,9 +34,34 @@ describe MetaMethodsDemo do
 
       hash = subject.locals_to_hash subject, s
 
-      hash[:a].should == 1
-      hash[:b].should == 2
-      hash[:c].should be_nil
+      expect(hash[:a]).to eq 1
+      expect(hash[:b]).to eq 2
+      expect(hash[:c]).to be_nil
     end
+  end
+
+  describe "#evaluate_dsl" do
+    it "calls all lambdas" do
+      create_block = lambda { created }
+      destroy_block = lambda {|object| p destroyed(object) }
+
+      execute_block = lambda {|object| p executed(object) }
+
+      subject.evaluate_dsl(create_block, destroy_block, execute_block)
+    end
+  end
+
+  private
+
+  def created
+    p "created"
+  end
+
+  def destroyed object
+    p "destroyed: #{object}"
+  end
+
+  def executed object
+    p "executed: #{object}"
   end
 end
